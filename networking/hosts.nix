90f26632a7b68b20cb46dddadb3307d3c9b56dbd
Walter Queiroz <walter@wcbrpar.com>
2025-07-11 20:52:52 -0400

    Separada a configuração de ID dos hosts no arquivo netwoking/hosts.nix

--- networking/hosts.nix
+++ networking/hosts.nix
@@ -1,13 +1,11 @@
 
 {
-  # Configuração de rede
-    
-    # hostId = "13960a97"; # Galactica            # cut -c-8 < /proc/sys/kernel/random/uuid    
-    # hostName = "galactica";
-    
-    hostId = "8bf0dda5"; # Pegasus
-    hostName = "pegasus";
+  # Naming-scheme - https://namingschemes.com/Battlestar_Galactica
 
-    # hostId = "e491eb5c"; # Yashuman
-    # hostName = "yashuman";
+  # hostId = "13960a97"; # Galactica      # cut -c-8 < /proc/sys/kernel/random/uuid    
+  # hostName = "galactica";
+  hostId = "8bf0dda5"; # Pegasus
+  hostName = "pegasus";
+  # hostId = "e491eb5c"; # Yashuman
+  # hostName = "yashuman";
 }
