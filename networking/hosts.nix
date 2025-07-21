6dde63e8deafe373cf477c4ffe8ecdc5dc57e95f
wjjunyor <wjjunyor@gmail.com>
2025-07-21 18:08:34 -0400

    Adicionado networking/hosts.nix do .gitignore

--- networking/hosts.nix
+++ networking/hosts.nix
@@ -2,10 +2,10 @@
 {
   # Naming-scheme - https://namingschemes.com/Battlestar_Galactica
 
-  # hostId = "13960a97"; # Galactica      # cut -c-8 < /proc/sys/kernel/random/uuid    
-  # hostName = "galactica";
-  hostId = "8bf0dda5"; # Pegasus
-  hostName = "pegasus";
+  hostId = "13960a97"; # Galactica      # cut -c-8 < /proc/sys/kernel/random/uuid    
+  hostName = "galactica";
+  # hostId = "8bf0dda5"; # Pegasus
+  # hostName = "pegasus";
   # hostId = "e491eb5c"; # Yashuman
   # hostName = "yashuman";
 }
