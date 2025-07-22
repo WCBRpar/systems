2f110d8d5dc46e0620c55d8a11c946e037bd1041
wjjunyor <wjjunyor@gmail.com>
2025-07-21 21:24:44 -0400

    OnlyOffice Document Server movido para o host Pegasus

--- modules/office/default.nix
+++ modules/office/default.nix
@@ -4,50 +4,53 @@
 
   environment.systemPackages = with pkgs; [ onlyoffice-documentserver ];
 
-  services = lib.mkIf ( config.networking.hostName == "galactica" ) {
+  services = {
 
-    traefik = {
+    traefik = lib.mkIf ( config.networking.hostName == "galactica" ) {
       dynamicConfigOptions = {
         http = {
-	  routers = {
-	    onlyoffice = {
-	      rule = "Host(`office.wcbrpar.com`) && (PathPrefix(`/`))";
-	      service = "onlyoffice-service";
-	      entrypoints = ["websecure"];
-	      tls = {
-		certResolver = "cloudflare";
-	      };
-	      # middlewares = ["onlyoffice-prefix"];
-	    };
-	  };
-	  services = {
-	    onlyoffice-service = {
-	      loadBalancer = {
-	        servers = [{ url = "http://127.0.0.1:8008"; }];
-		passHostHeader = true;
-		healthCheck = {
-		  path = "/healthcheck/";
-		  interval = "10s";
-		  timeout = "3s";
-		};
-	      };
-	    };
-	  };
-	  middlewares = {
-	    onlyoffice-prefix = {
-	      stripPrefix.prefixes = ["/"];
-	    };
-	  };
-	};
+          routers = {
+            onlyoffice = {
+              # rule = "Host(`office.wcbrpar.com`)";
+              rule = "Host(`office.wcbrpar.com`) && (PathPrefix(`/`))";
+              service = "onlyoffice-service";
+              entrypoints = ["websecure"];
+              tls = {
+                certResolver = "cloudflare";
+              };
+              middlewares = ["onlyoffice-prefix"];
+            };
+          };
+          services = {
+            onlyoffice-service = {
+              loadBalancer = {
+                servers = [{ url = "http://192.168.13.20:8008"; }];
+                passHostHeader = true;
+                healthCheck = {
+                  path = "/healthcheck/";
+                  interval = "10s";
+                  timeout = "3s";
+                };
+              };
+            };
+          };
+          middlewares = {
+            onlyoffice-prefix = {
+              stripPrefix.prefixes = ["/"];
+            };
+          };
+        };
       };
     };
 
-    onlyoffice = {
+    onlyoffice = lib.mkIf ( config.networking.hostName == "pegasus" )  {
       port = 8008;
       enable = true;
       hostname = "office.wcbrpar.com";
+      enableExampleServer = true;
     };
-    nginx.virtualHosts."office.wcbrpar.com" = {
+
+    nginx.virtualHosts."office.wcbrpar.com" = lib.mkIf ( config.networking.hostName == "pegasus" ) {
       extraConfig = ''
         # Force nginx to return relative redirects. This lets the browser
         # figure out the full URL. This ends up working better because it's in
@@ -56,12 +59,11 @@
       '';
       listen = [
         {
-          port = 8009;
+          port = 8008;
           addr = "127.0.0.1";
         }
       ];
     };
   };
-
 
 }
