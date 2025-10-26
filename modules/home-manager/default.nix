5b3ccb0ef61021d5bcaeafcd71e7ab47f0998ce9
wjjunyor <wjjunyor@gmail.com>
2025-10-26 14:54:11 -0400

    Configuração de condicional por hosts para o home-manager

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,5 +1,9 @@
 { config, pkgs, lib, ... }:
 
+
+let
+  hostname = builtins.readFile /etc/hostname;
+in 
 {
 
   # Confugurções do Home-Manager
@@ -12,6 +16,9 @@
   home-manager.users.wjjunyor = { pkgs, ... }: {
     imports = [
       /home/wjjunyor/.config/home-manager/home.nix
+      /home/wjjunyor/.config/home-manager/hosts/common.nix
+      (/home/wjjunyor/.config/home-manager/hosts + "/${builtins.replaceStrings ["\n"] [""] hostname}.nix")
+
       # (builtins.fetchGit {
       #   url = "git@github.com:wjjunyor/nixos-home-manager.git";
       #   ref = "main";  # Nome do branch
