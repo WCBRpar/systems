8968a8772cdba4e51fe4e8fa2c242ffae62f16c7
Walter Queiroz <wjjunyor@gmail.com>
2026-01-06 22:25:05 -0400

    Desativado filesharing e depurando

--- modules/default.nix
+++ modules/default.nix
@@ -7,7 +7,7 @@
     ./dns
     # ./crm
     ./editor
-    ./filesharing
+    # ./filesharing
     # ./gitsync = REFORMAR
     ./home-manager
     ./iam
--- modules/filesharing/default.nix
+++ modules/filesharing/default.nix
@@ -1,13 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
-  fileSystems."/nas-data/home" = {
+  fileSystems."/nas-data/home" = lib.mkIf ( config.networking.hostName <> "yashuman" ) {
     device = "172.16.129.26:/nas/6116/users";
     fsType = "nfs";
     options = [ "nofail" ];
   };
 
-  services.nfs.server = lib.mkIf (config.networking.hostName == "galactica") {
+  services.nfs.server = lib.mkIf (config.networking.hostName == "yashuman") {
     enable = true;
     exports = ''
       /nas-data/home   192.168.13.0/24(rw,fsid=0,no_subtree_check)
