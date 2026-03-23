b60c32ac334f1a5184176c3239ba0ced516d189f
wjjunyor <wjjunyor@gmail.com>
2026-03-23 12:58:11 -0400

    Ativação do módulo FileSharing

--- modules/filesharing/default.nix
+++ modules/filesharing/default.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  fileSystems."/nas-data/home" = lib.mkIf ( config.networking.hostName <> "yashuman" ) {
+  fileSystems."/nas-data/home" = {
     device = "172.16.129.26:/nas/6116/users";
     fsType = "nfs";
     options = [ "nofail" ];
@@ -10,8 +10,8 @@
   services.nfs.server = lib.mkIf (config.networking.hostName == "yashuman") {
     enable = true;
     exports = ''
-      /nas-data/home     192.168.13.0/24(rw,fsid=0,no_subtree_check)
-      /nas-data/shared   192.168.13.0/24(rw,fsid=0,no_subtree_check)
+      /nas-data/home     192.168.13.0/24(rw, sync, no_subtree_check, fdid=1)
+      /nas-data/shared   192.168.13.0/24(rw, sync, no_subtree_check, fsid=2)
     '';
   };
 
