d2e212ec397766f53c91128d161712df9c053e8d
wjjunyor <wjjunyor@gmail.com>
2026-04-25 23:49:39 -0400

    feature: Adicionada a Workstation REDPAD002 com nix-on-droid

--- workstations/common.nix
+++ workstations/common.nix
@@ -1,21 +1,25 @@
 {
   config,
   pkgs,
+  lib,
+  inputs,
   ...
 }:
 {
-  networking.hostId = "d00dbeef"; # ID único para T800
-  networking.hostName = "t800";
 
-  imports = [
-    ./default.nix
-  ];
+  # Configurações de rede
+  networking.networkmanager.enable = true; 
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
 
-  # Configurações específicas do hardware (já incluídas em workstations/default.nix via nixos-hardware)
-  # nixos-hardware/lenovo/ideapad/s145-15api
-
-  # Usuário caroles (referenciado do LDAP, não criado localmente)
-  # A configuração do usuário 'caroles' e home-manager está em workstations/default.nix
-
-  # Outras configurações específicas para T800, se houver
+  # Auto-upgrade do sistema
+  system = {
+    autoUpgrade.enable = true;
+    stateVersion = "24.11";
+  };
 }
--- workstations/default.nix
+++ workstations/default.nix
@@ -10,21 +10,7 @@
     inputs.home-manager.nixosModules.home-manager
     ../users
     ../modules/home-manager
+    ./common.nix
   ];
 
-  # Configurações de rede
-  networking.networkmanager.enable = true; 
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
-    };
-  };
-
-  # Auto-upgrade do sistema
-  system = {
-    autoUpgrade.enable = true;
-    stateVersion = "24.11";
-  };
 }
